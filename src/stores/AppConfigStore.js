3bf0fb0f69da919c971e86aabb9943b6fadd22f0
Eamon Hyland <ehyland90@gmail.com>
2015-12-06 11:33:20 +0000

    Created AppConfigStore

---
+++ src/stores/AppConfigStore.js
@@ -0,0 +1,67 @@
+import BaseStore from "fluxible/addons/BaseStore";
+import Actions from "../constants/Actions";
+
+class AppConfigStore extends BaseStore {
+
+  static storeName = "AppConfigStore";
+
+  static handlers = {
+    [Actions.LOAD_APP_CONFIG_SUCCESS]: "handleReceiveAppConfig"
+  }
+
+  constructor (dispatcher) {
+    super(dispatcher);
+    this.configFetched = false;
+    this.config = {
+      siteTitle: "",
+      metaDescription: "",
+      metaKeywords: "",
+      noEventsText: ["No Events"],
+      noEventsFormURL: ""
+    };
+  }
+
+  /**
+   * Dispatch handlers
+   */
+  handleReceiveAppConfig ({DefaultSiteTitle = "", DefaultMetaDescription = "", DefaultMetaKeywords = "", NoEventsText = "", NoEventsFormURL = ""}) {
+
+    this.configFetched = true;
+
+    this.config.siteTitle = DefaultSiteTitle,
+    this.config.metaDescription = DefaultMetaDescription,
+    this.config.metaKeywords = DefaultMetaKeywords,
+    this.config.noEventsText = NoEventsText.split("\n"),
+    this.config.noEventsFormURL = NoEventsFormURL
+
+    this.emitChange();
+  }
+
+  /**
+   * Getters
+   */
+  getConfig () {
+    return this.config;
+  }
+
+  hasConfigBeenFetched () {
+    return this.configFetched;
+  }
+
+  /**
+   * Transferring state
+   */
+  dehydrate () {
+    return {
+      configFetched: this.configFetched,
+      config: this.config
+    };
+  }
+
+  rehydrate (state) {
+    this.configFetched = state.configFetched;
+    this.config = state.config;
+  }
+}
+
+export default AppConfigStore;
