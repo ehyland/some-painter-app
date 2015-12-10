3350a665bad28c224ed9d30581893cbd35e6d344
Eamon Hyland <ehyland90@gmail.com>
2015-12-10 23:31:44 +0000

    Updated cms.baseURL to live domain

--- src/configs/prod.conf.js
+++ src/configs/prod.conf.js
@@ -1,6 +1,6 @@
 export default {
   cms: {
-    baseURL: "http://128.199.84.238:8888/"
+    baseURL: "http://cms.somepainter.com/"
   },
   googleAnalyticsCode: "UA-64561324-1",
   fbAppId: "199112500427767"
